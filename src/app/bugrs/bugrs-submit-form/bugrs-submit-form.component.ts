b97d44f0b7c1589a37508e1049e5ef1ac3a66c5b
Troumpoulos <s.troumpoulos@accenture.com>
2019-12-14 14:27:47 +0200

    POST functionallity

--- src/app/bugrs/bugrs-submit-form/bugrs-submit-form.component.ts
+++ src/app/bugrs/bugrs-submit-form/bugrs-submit-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { BugrsRetrievalService } from '../bugrs-system/bugrs-retrieval.service';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'brs-bugrs-submit-form',
@@ -14,7 +17,7 @@ export class BugrsSubmitFormComponent implements OnInit {
   reporterList: Array<String> = ["QA","PO","DEV"];
   statusList: Array<string> = ["Ready for Test","Done","Rejected"];
 
-  constructor() { }
+  constructor(private bugService: BugrsRetrievalService, private router: Router) { }
 
   ngOnInit() {
     this.submitForm = new FormGroup({
@@ -24,5 +27,15 @@ export class BugrsSubmitFormComponent implements OnInit {
       reporter: new FormControl(),
       status: new FormControl()
     });
+  }
+
+  addEditBug() {
+    if (this.submitForm.invalid) {
+      return;
+    }
+
+    this.bugService.createBug(this.submitForm.value).pipe(
+      tap(() => this.router.navigate(['']))
+    ).subscribe();
   }
 }
