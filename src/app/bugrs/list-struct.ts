e093b3daec17c0797ecbe966681b8155e219d90a
Spachidis <dimitrios.spachidis@accenture.com>
2020-01-06 23:41:38 +0200

    list of comments

--- src/app/bugrs/list-struct.ts
+++ src/app/bugrs/list-struct.ts
@@ -1,3 +1,5 @@
+import { bugComment } from "./comment-struct";
+
 export interface ListStruct {
   id: string;
   title: string;
@@ -6,6 +8,6 @@ export interface ListStruct {
   createdAt: string;
   updatedAt: string;
   status: string;
-
-
+  comments: bugComment[];
 }
+
