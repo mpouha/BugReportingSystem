608810798c8ccb62ac59dbfc628439c51e10f65d
Troumpoulos <s.troumpoulos@accenture.com>
2020-01-08 13:58:48 +0200

    add footer

--- src/app/common-modules/common-modules.module.ts
+++ src/app/common-modules/common-modules.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatusBarComponent } from './status-bar/status-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [StatusBarComponent],
+  declarations: [StatusBarComponent, FooterComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    StatusBarComponent
+    StatusBarComponent,
+    FooterComponent
   ]
 })
 export class CommonModulesModule { }
